72a5c51ef6796f79e17c7a0daee149ab8551fa81
Alessandro Salerno <67583725+Alessandro-Salerno@users.noreply.github.com>
2025-04-12 10:49:18 +0200

    Update version.c

--- src/common/cli/directives/commands/version.c
+++ src/common/cli/directives/commands/version.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 | tarman                                                                 |
-| Copyright (C) 2024 - 2025 Alessandro Salerno                                  |
+| Copyright (C) 2024 - 2025 Alessandro Salerno                           |
 |                                                                        |
 | This program is free software: you can redistribute it and/or modify   |
 | it under the terms of the GNU General Public License as published by   |
